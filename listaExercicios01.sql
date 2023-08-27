b8f4cbe98e309c2ea9b31bbf08799a8431990a12
Hector-pellegrino <102663611+Hector-pellegrino@users.noreply.github.com>
2023-08-27 16:13:08 -0300

    Update listaExercicios01.sql

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -5,3 +5,8 @@ select nome from autores where nascimento < '1900-00-00';
 select livros.titulo, autores.nome from livros
 inner join autores 
 on livros.autor_id = autores.id and autores.nome = 'J.K. Rowling';
+
+select alunos.nome, matriculas.curso
+from alunos
+inner join matriculas 
+on alunos.id = matriculas.aluno_id and matriculas.curso = 'Engenharia de Software';
