dbff21701294acaa40f2f93a67c7068e10e00254
Hector-pellegrino <102663611+Hector-pellegrino@users.noreply.github.com>
2023-08-27 15:50:44 -0300

    Update listaExercicios01.sql

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,2 +1,7 @@
 select * from livros;
+
 select nome from autores where nascimento < '1900-00-00';
+
+select livros.titulo, autores.nome from livros
+inner join autores 
+on livros.autor_id = autores.id and autores.nome = 'J.K. Rowling';
