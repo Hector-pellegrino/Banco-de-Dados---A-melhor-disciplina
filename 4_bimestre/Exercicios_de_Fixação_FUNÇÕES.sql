3e7f6e098d4fc4c7c5ff3d44db71a18a2333e7e0
Hector-pellegrino <102663611+Hector-pellegrino@users.noreply.github.com>
2023-10-09 18:22:25 -0300

    Exercicio 2

--- 4_bimestre/Exercicios_de_Fixação_FUNÇÕES.sql
+++ 4_bimestre/Exercicios_de_Fixação_FUNÇÕES.sql
@@ -9,3 +9,7 @@ insert into nomes
 values ('Roberta'), ('Roberto'), ('Maria Clara'), ('João');
 
 select upper(nome) from nomes;
+
+select length(nome) from nomes;
+
+select concat('Sr.', nome) as masculino from nomes where nome like '%o' ;
