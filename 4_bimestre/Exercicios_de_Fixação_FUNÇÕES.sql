74fcb524b59e0a11add3d19c9a25b5b6796b84ad
Hector-pellegrino <102663611+Hector-pellegrino@users.noreply.github.com>
2023-10-09 22:40:27 -0300

    ex 2

--- 4_bimestre/Exercicios_de_Fixação_FUNÇÕES.sql
+++ 4_bimestre/Exercicios_de_Fixação_FUNÇÕES.sql
@@ -13,3 +13,26 @@ select upper(nome) from nomes;
 select length(nome) from nomes;
 
 select concat('Sr.', nome) as masculino from nomes where nome like '%o' ;
+
+-- exercicio 2 --
+
+CREATE TABLE produtos (
+    produto VARCHAR(50),
+    preco DECIMAL(6, 2),
+    quantidade INT
+);
+
+INSERT INTO produtos
+VALUES
+    ('Chuteira', 599.99, 40),
+    ('Camisa Palmeiras', 399.99, 50),
+    ('short Palmeiras', 75.50, 100),
+    ('Meiao Palmeiras', 49.99, 30);
+
+SELECT produto, ROUND(preco, 2) AS preço FROM produtos;
+
+SELECT ABS(quantidade) AS quantidade_abs FROM produtos;
+
+SELECT AVG(preco) AS media FROM produtos;
+
+
